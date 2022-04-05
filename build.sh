d1de8a3f3467e3f423737f7b5ada1182a157b4fd
Ron Kitay <ronk@qwilt.com>
2022-04-05 12:25:43 +0300

    make build.sh work from root dir + ensure image building does not use cache

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+cd $(dirname $0)
+
 BUILD_ARGS=$(cat $(git rev-parse --show-toplevel)/versions | awk '{print " --build-arg "$0" "}')
 
 export DOCKER_BUILDKIT=0
 
-docker build ${BUILD_ARGS} -t $(basename $(pwd)):$(cat $(pwd)/release) .
+docker build ${BUILD_ARGS} --no-cache -t $(basename $(pwd)):$(cat $(pwd)/release) .
