6ca91824dcd7dabdf3635b10cb6aafa4ba455507
Ron Kitay <ronk@qwilt.com>
2022-04-05 12:08:05 +0300

    unify build.sh

--- docker-eks/build.sh
+++ docker-eks/build.sh
@@ -1,7 +1,1 @@
-#!/bin/sh
-
-BUILD_ARGS=$(cat $(git rev-parse --show-toplevel)/versions | awk '{print " --build-arg "$0" "}')
-
-export DOCKER_BUILDKIT=0
-
-docker build ${BUILD_ARGS} -t $(basename $(pwd)):$(cat $(pwd)/release) .
+../build.sh
