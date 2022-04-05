aa45e610e892fe6a02a0d2730cd11b820ea905ed
Ron Kitay <ronk@qwilt.com>
2022-04-05 12:08:30 +0300

    remove unneeded run.sh from docker-eks

--- docker-eks/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run \
-    -v /Users/ronk/.aws/:/root/.aws \
-    -v /Users/ronk/.my-docker-envs/eks-dev/.kube:/root/.kube \
-    --env AWS_PROFILE=rnd \
-    --env AWS_REGION=eu-west-1 \
-    --env EKS_CLUSTER=EKSClusterDev \
-    -t -i --rm $(basename $(pwd)):v1 sh -l
