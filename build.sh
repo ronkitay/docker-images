339746b28b79308f64c7805ebe59bb2ecf74378e
Ron Kitay <ronk@qwilt.com>
2022-04-07 20:16:23 +0300

    disable using cache during build and support a prepare script per image

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 
 cd $(dirname $0)
 
+if [[ -f prepare.sh ]]; 
+then
+    ./prepare.sh
+fi
+
 BUILD_ARGS=$(cat $(git rev-parse --show-toplevel)/versions | awk '{print " --build-arg "$0" "}')
 
 export DOCKER_BUILDKIT=0
 
-docker build ${BUILD_ARGS} --no-cache -t $(basename $(pwd)):$(cat $(pwd)/release) .
+docker build ${BUILD_ARGS} -t $(basename $(pwd)):$(cat $(pwd)/release) .
