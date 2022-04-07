0cd02a6f79a844a740a1f0bce22d58a5248bea6d
Ron Kitay <ronk@qwilt.com>
2022-04-07 20:49:00 +0300

    cleanup

--- basic-env/prepare.sh
+++ basic-env/prepare.sh
@@ -5,7 +5,3 @@ cat ${HOME}/.fzf.settings >> .zshrc.generated
 cat .oh.my.zsh.settings >> .zshrc.generated
 
 echo "" >> .zshrc.generated
-
-#cp /Users/ronk/.my-docker-envs/Workspace-1649318775/.p10k.zsh .p10k.zsh
-# cp ${HOME}/.p10k.zsh .p10k.zsh
-#cp /Users/ronk/.my-docker-envs/Workspace-1649323400/.p10k.zsh .p10k.zsh
