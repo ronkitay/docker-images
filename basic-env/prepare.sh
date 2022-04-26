758c45e6228e827f390486d8a485b473189ebf7f
Ron Kitay <ronk@qwilt.com>
2022-04-26 15:25:23 +0300

    Fix grep to support --color + add all my aliases to basic-env

--- basic-env/prepare.sh
+++ basic-env/prepare.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-cat ${HOME}/.bindkey.settings > .zshrc.generated
+echo "" > .zshrc.generated
+
+
+cat ${HOME}/.bindkey.settings >> .zshrc.generated
 cat ${HOME}/.fzf.settings >> .zshrc.generated
 cat .oh.my.zsh.settings >> .zshrc.generated
+
+cat ${HOME}/.aliases/.common.aliases >> .zshrc.generated
+cat ${HOME}/.aliases/.git.aliases >> .zshrc.generated
+cat ${HOME}/.aliases/.iterm.aliases >> .zshrc.generated
+cat ${HOME}/.aliases/.java.aliases >> .zshrc.generated
+cat ${HOME}/.aliases/.k8s.aliases >> .zshrc.generated
 
 echo "" >> .zshrc.generated
