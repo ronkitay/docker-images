b8a8ab90a54412a638eb81ba40bf1baaefa33e41
Ron Kitay <ronkitay@gmail.com>
2025-08-10 09:15:04 +0300

    some scripting fixes

--- build-module.sh
+++ build-module.sh
@@ -1,1 +1,5 @@
+export DOCKERHUB_USERNAME=$(cat $(REPO_ROOT)/.dockerhub_username)
+export DOCKERHUB_TOKEN=$(cat $(REPO_ROOT)/.dockerhub_password)
+export ARCHITECTURE=$(docker version --format '{{.Server.Arch}}')
+
 make $1 -B -s
